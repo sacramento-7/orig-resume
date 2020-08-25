451fee6553eb20a1ef83411b15f0f938f651c079
Hossein Kashfinia <hossein.kashfinia@gmail.com>
2020-08-26 01:12:45 +0430

    Updates

---
+++ precache-manifest.31af243cedf1b7a8390173c692b1acb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ca36b978f7800ae34529a9e4565597",
+    "url": "/orig-resume/index.html"
+  },
+  {
+    "revision": "b11182ffd617151b8da1",
+    "url": "/orig-resume/static/css/main.b2961d32.chunk.css"
+  },
+  {
+    "revision": "ba2422cf1334f92b28e7",
+    "url": "/orig-resume/static/js/2.f399b5ed.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/orig-resume/static/js/2.f399b5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b11182ffd617151b8da1",
+    "url": "/orig-resume/static/js/main.32b8ad2d.chunk.js"
+  },
+  {
+    "revision": "f468a5bfdeff7f36d32e",
+    "url": "/orig-resume/static/js/runtime-main.8e7a7ad7.js"
+  }
+]);
